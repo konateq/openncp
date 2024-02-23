19ffa0f4b53242234cb9211321ed722885102985
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-02-23 15:43:26 +0100

    [EHEALTH-11140] Add REST API part to openncp-api-general

---
+++ openncp-api/openncp-api-general/src/main/java/eu/europa/ec/sante/openncp/api/general/transformation/TransformationApplication.java
@@ -0,0 +1,30 @@
+package eu.europa.ec.sante.openncp.api.general.transformation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(scanBasePackages = {"eu.europa.ec.sante.openncp.transformation"})
+@EnableSwagger2
+public class TransformationApplication extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransformationApplication.class);
+
+    public static void main(String... args) {
+        SpringApplication.run(TransformationApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //return configureApplication(builder);
+        return builder;
+    }
+
+    //private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+    //  return builder.sources(TranslationsAndMappingsApplication.class).bannerMode(Banner.Mode.OFF);
+    //}
+}
