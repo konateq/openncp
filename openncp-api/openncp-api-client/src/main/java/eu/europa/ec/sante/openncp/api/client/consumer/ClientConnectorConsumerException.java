bc8c58d78f4e45a7f476d649c622727254c9c36e
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-03-12 15:18:13 +0100

    Latest version

---
+++ openncp-api/openncp-api-client/src/main/java/eu/europa/ec/sante/openncp/api/client/consumer/ClientConnectorConsumerException.java
@@ -0,0 +1,33 @@
+package eu.europa.ec.sante.openncp.api.client.consumer;
+
+
+import eu.europa.ec.sante.openncp.common.error.OpenNCPErrorCode;
+
+public class ClientConnectorConsumerException extends Exception {
+
+
+    private OpenNCPErrorCode openncpErrorCode;
+    String context;
+
+    public ClientConnectorConsumerException(OpenNCPErrorCode openncpErrorCode, String message, String context) {
+        super(message);
+        this.openncpErrorCode = openncpErrorCode;
+        this.context = context;
+    }
+
+    public ClientConnectorConsumerException(OpenNCPErrorCode openncpErrorCode, String message, String context, Throwable cause) {
+        super(message, cause);
+        this.openncpErrorCode = openncpErrorCode;
+        this.context = context;
+    }
+
+    public OpenNCPErrorCode getOpenncpErrorCode() {
+        return openncpErrorCode;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+
+}
