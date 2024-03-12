e1398b4116a75447cd710ca00ff1d0b302152346
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2024-03-12 16:07:55 +0100

    Buildable project

--- openncp-core/openncp-core-client/src/main/java/eu/europa/ec/sante/openncp/core/client/ihe/ClientConnectorServiceImpl.java
+++ openncp-core/openncp-core-client/src/main/java/eu/europa/ec/sante/openncp/core/client/ihe/ClientConnectorServiceImpl.java
@@ -1,12 +1,44 @@
-package eu.europa.ec.sante.openncp.api.client.connector;
+package eu.europa.ec.sante.openncp.core.client.ihe;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import javax.xml.ws.Service;
 
 import eu.europa.ec.sante.openncp.common.ClassCode;
 import eu.europa.ec.sante.openncp.common.NcpSide;
 import eu.europa.ec.sante.openncp.common.validation.OpenNCPValidation;
-import eu.europa.ec.sante.openncp.core.client.*;
+import eu.europa.ec.sante.openncp.core.client.ClientConnectorService;
+import eu.europa.ec.sante.openncp.core.client.ClientConnectorServicePortType;
+import eu.europa.ec.sante.openncp.core.client.DocumentId;
+import eu.europa.ec.sante.openncp.core.client.EpsosDocument;
+import eu.europa.ec.sante.openncp.core.client.FilterParams;
+import eu.europa.ec.sante.openncp.core.client.GenericDocumentCode;
+import eu.europa.ec.sante.openncp.core.client.ObjectFactory;
+import eu.europa.ec.sante.openncp.core.client.PatientDemographics;
+import eu.europa.ec.sante.openncp.core.client.PatientId;
+import eu.europa.ec.sante.openncp.core.client.QueryDocumentRequest;
+import eu.europa.ec.sante.openncp.core.client.QueryDocumentsResponse;
+import eu.europa.ec.sante.openncp.core.client.QueryPatientRequest;
+import eu.europa.ec.sante.openncp.core.client.QueryPatientResponse;
+import eu.europa.ec.sante.openncp.core.client.RetrieveDocumentRequest;
+import eu.europa.ec.sante.openncp.core.client.RetrieveDocumentResponse;
+import eu.europa.ec.sante.openncp.core.client.SubmitDocumentRequest;
+import eu.europa.ec.sante.openncp.core.client.SubmitDocumentResponse;
 import eu.europa.ec.sante.openncp.core.client.ihe.cxf.interceptor.AssertionsInInterceptor;
-import eu.europa.ec.sante.openncp.core.client.ihe.dts.*;
-import eu.europa.ec.sante.openncp.core.client.ihe.service.*;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.DocumentDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.FilterParamsDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.GenericDocumentCodeDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.PatientDemographicsDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.PatientIdDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.RetrieveDocumentResponseDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.dts.XdsDocumentDts;
+import eu.europa.ec.sante.openncp.core.client.ihe.service.DispensationService;
+import eu.europa.ec.sante.openncp.core.client.ihe.service.IdentificationService;
+import eu.europa.ec.sante.openncp.core.client.ihe.service.OrCDService;
+import eu.europa.ec.sante.openncp.core.client.ihe.service.OrderService;
+import eu.europa.ec.sante.openncp.core.client.ihe.service.PatientService;
 import eu.europa.ec.sante.openncp.core.client.ihe.xdr.XdrResponse;
 import eu.europa.ec.sante.openncp.core.client.logging.LoggingSlf4j;
 import eu.europa.ec.sante.openncp.core.common.assertionvalidator.constants.AssertionEnum;
@@ -27,12 +59,6 @@ import org.opensaml.saml.saml2.core.Assertion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.ws.Service;
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 @javax.jws.WebService(
         serviceName = "ClientConnectorService",
         portName = "ClientConnectorServicePort",
@@ -48,15 +74,18 @@ public class ClientConnectorServiceImpl  implements ClientConnectorServicePortTy
 
     private ObjectFactory objectFactory = new ObjectFactory();
 
+    final ClientConnectorServicePortType port;
+
+    final Client client;
 
     public ClientConnectorServiceImpl() {
-//        Service service = ClientConnectorService.create(ClientConnectorService.SERVICE);
-//        port = service.getPort(ClientConnectorServicePortType.class);
-//        client = ClientProxy.getClient(port);
-//        Endpoint endpoint = client.getEndpoint();
-//        List<Interceptor<? extends Message>> inInterceptors = endpoint.getInInterceptors();
-//        AssertionsInInterceptor assertionsInInterceptor = new AssertionsInInterceptor();
-//        inInterceptors.add(assertionsInInterceptor);
+        Service service = ClientConnectorService.create(ClientConnectorService.SERVICE);
+        port = service.getPort(ClientConnectorServicePortType.class);
+        client = ClientProxy.getClient(port);
+        Endpoint endpoint = client.getEndpoint();
+        List<Interceptor<? extends Message>> inInterceptors = endpoint.getInInterceptors();
+        AssertionsInInterceptor assertionsInInterceptor = new AssertionsInInterceptor();
+        inInterceptors.add(assertionsInInterceptor);
     }
 
 
