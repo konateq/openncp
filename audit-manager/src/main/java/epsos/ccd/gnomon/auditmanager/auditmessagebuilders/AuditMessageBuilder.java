4b983401798c37bf587c31a2ecde42adbadd860b
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2024-05-07 14:39:49 +0200

    EHEALTH-11396: Make the OpenNCP web-manager work with the new L&S code

--- audit-manager/src/main/java/epsos/ccd/gnomon/auditmanager/auditmessagebuilders/AuditMessageBuilder.java
+++
@@ -1,9 +0,0 @@
-package epsos.ccd.gnomon.auditmanager.auditmessagebuilders;
-
-import epsos.ccd.gnomon.auditmanager.EventLog;
-import net.RFC3881.AuditMessage;
-
-public interface AuditMessageBuilder {
-
-    AuditMessage build(EventLog eventLog);
-}
