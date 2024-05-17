0b6941e27444c2736c46cf90f93f93c845101ec7
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2024-05-17 16:30:29 +0200

    EHEALTH-11396: Make the OpenNCP web-manager work with the new L&S code

--- openncp-web-manager/openncp-web-manager-backend/src/main/java/eu/europa/ec/sante/openncp/webmanager/backend/security/TokenProvider.java
+++ openncp-web-manager/openncp-web-manager-backend/src/main/java/eu/europa/ec/sante/openncp/webmanager/backend/security/TokenProvider.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import java.util.Date;
 
 @Component
