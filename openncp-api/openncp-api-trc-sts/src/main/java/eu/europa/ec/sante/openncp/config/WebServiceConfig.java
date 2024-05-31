79eb640df493be9772f7bbf0bb97c5e32b927fda
Kim Wauters <kim.wauters@ext.ec.europa.eu>
2024-05-31 12:02:02 +0200

    - Set the org.apache.cxf.stax.allowInsecureParser property for the TRC-STS application to true because of a very vague error "Cannot create a secure XMLInputFactory"

--- openncp-api/openncp-api-trc-sts/src/main/java/eu/europa/ec/sante/openncp/config/WebServiceConfig.java
+++ openncp-api/openncp-api-trc-sts/src/main/java/eu/europa/ec/sante/openncp/config/WebServiceConfig.java
@@ -1,31 +1,35 @@
 package eu.europa.ec.sante.openncp.config;
 
-import javax.xml.ws.Endpoint;
-
 import eu.europa.ec.sante.openncp.sts.NextOfKinService;
 import eu.europa.ec.sante.openncp.sts.STSService;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.xml.ws.Endpoint;
 
 @Configuration
 public class WebServiceConfig {
 
     private static final String BINDING_URI = "http://www.w3.org/2003/05/soap/bindings/HTTP/";
 
-    @Bean(name=Bus.DEFAULT_BUS_ID)
+    @Bean(name = Bus.DEFAULT_BUS_ID)
     public SpringBus springBus() {
-        var loggingFeature = new LoggingFeature();
-        loggingFeature.setPrettyLogging(true);
-
         var springBus = new SpringBus();
-        springBus.getFeatures().add(loggingFeature);
+        springBus.getFeatures().add(new LoggingFeature());
         return springBus;
     }
- 
+
+//    @Bean
+//    public JaxWsProxyFactoryBean jaxWsProxyFactoryBean() {
+//        final JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+//        factory.setFeatures(Collections.singletonList(new LoggingFeature()));
+//        return factory;
+//    }
+
     @Bean
     public Endpoint endpoint(Bus bus, STSService stsService) {
         EndpointImpl endpoint = new EndpointImpl(bus, stsService);
