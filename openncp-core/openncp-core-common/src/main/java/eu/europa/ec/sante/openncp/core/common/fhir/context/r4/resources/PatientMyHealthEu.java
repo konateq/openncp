42cb217067fbcb864aea5b32357cc6d82f49fc59
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-06-17 10:58:45 +0200

    Fix extend

--- openncp-core/openncp-core-common/src/main/java/eu/europa/ec/sante/openncp/core/common/fhir/context/r4/resources/PatientMyHealthEu.java
+++ openncp-core/openncp-core-common/src/main/java/eu/europa/ec/sante/openncp/core/common/fhir/context/r4/resources/PatientMyHealthEu.java
@@ -1,10 +1,10 @@
 package eu.europa.ec.sante.openncp.core.common.fhir.context.r4.resources;
 
 import ca.uhn.fhir.model.api.annotation.ResourceDef;
-import org.hl7.fhir.r4.model.ServiceRequest;
+import org.hl7.fhir.r4.model.Patient;
 
 @ResourceDef(name = "PatientMyHealthEu", profile = PatientMyHealthEu.PROFILE)
-public class PatientMyHealthEu extends ServiceRequest {
+public class PatientMyHealthEu extends Patient {
 
     public static final String PROFILE = "http://fhir.ehdsi.eu/laboratory/StructureDefinition/Patient-lab-myhealtheu";
 }
