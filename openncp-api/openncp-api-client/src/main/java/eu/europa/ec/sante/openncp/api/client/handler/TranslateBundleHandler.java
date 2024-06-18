b35e6d34c408e0085def7e5a8be1d95224920bb7
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-06-18 10:00:23 +0200

    [EHEALTH-11067] Log errors and warnings encountered in translations/mappings

--- openncp-api/openncp-api-client/src/main/java/eu/europa/ec/sante/openncp/api/client/handler/TranslateBundleHandler.java
+++ openncp-api/openncp-api-client/src/main/java/eu/europa/ec/sante/openncp/api/client/handler/TranslateBundleHandler.java
@@ -1,26 +1,56 @@
 package eu.europa.ec.sante.openncp.api.client.handler;
 
 import eu.europa.ec.sante.openncp.api.common.handler.BundleHandler;
+import eu.europa.ec.sante.openncp.common.Constant;
+import eu.europa.ec.sante.openncp.common.configuration.ConfigurationManager;
 import eu.europa.ec.sante.openncp.core.common.fhir.transformation.domain.TMResponseStructure;
 import eu.europa.ec.sante.openncp.core.common.fhir.transformation.service.impl.TranslationService;
 import org.apache.commons.lang3.Validate;
 import org.hl7.fhir.r4.model.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class TranslateBundleHandler implements BundleHandler {
 
-    private final TranslationService translationService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TranslateBundleHandler.class);
 
-    public TranslateBundleHandler(final TranslationService translationService) {
+    private final TranslationService translationService;
+    private final ConfigurationManager configurationManager;
+
+    public TranslateBundleHandler(final TranslationService translationService, final ConfigurationManager configurationManager) {
         this.translationService = Validate.notNull(translationService);
+        this.configurationManager = configurationManager;
+
     }
 
     @Override
     public Bundle handle(final Bundle bundle) {
-        final TMResponseStructure translatedBundle = translationService.translate(bundle, "en-GB");
-        //todo error handling
+        final String targetLanguage = configurationManager.getProperty(Constant.LANGUAGE_CODE);
+        LOGGER.info("Translating FHIR bundle from MyHealth@EU format into national language [{}]", targetLanguage);
+        final TMResponseStructure translatedBundle = translationService.translate(bundle, targetLanguage);
+        Validate.notNull(translatedBundle);
+        logErrors(translatedBundle);
+        logWarnings(translatedBundle);
+
         return translatedBundle.getFhirDocument();
+    }
+
+    private static void logErrors(final TMResponseStructure translatedBundle) {
+        if (!translatedBundle.getErrors().isEmpty()) {
+            for (final String error : translatedBundle.getErrors()) {
+                LOGGER.error(error);
+            }
+        }
+    }
+
+    private static void logWarnings(final TMResponseStructure translatedBundle) {
+        if (!translatedBundle.getWarnings().isEmpty()) {
+            for (final String warning : translatedBundle.getWarnings()) {
+                LOGGER.warn(warning);
+            }
+        }
     }
 }
