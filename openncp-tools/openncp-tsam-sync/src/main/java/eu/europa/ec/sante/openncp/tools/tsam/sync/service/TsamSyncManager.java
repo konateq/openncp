a398f7311275b97ea684841efe54b73f940b37a2
Alberto MIORI <alberto.miori@ext.ec.europa.eu>
2024-07-09 20:33:25 +0200

    Update TsamSyncManager.java

--- openncp-tools/openncp-tsam-sync/src/main/java/eu/europa/ec/sante/openncp/tools/tsam/sync/service/TsamSyncManager.java
+++ openncp-tools/openncp-tsam-sync/src/main/java/eu/europa/ec/sante/openncp/tools/tsam/sync/service/TsamSyncManager.java
@@ -150,8 +150,6 @@ public class TsamSyncManager {
 
                     total += concepts.size();
                     hasNext = concepts.size() == pageSize;
-
-                    break;
                 }
                 logger.info("{}/{}: '{}' completed ({} concepts with '{}' mappings)", index++, catalogue.getValueSetVersions().size(),
                         valueSetVersionModel.getValueSet().getName(), total, numberOfMapping);
