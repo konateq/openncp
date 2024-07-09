c359cb4b58951be0c1cd62316053012cb3460b3b
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-07-09 13:09:32 +0200

    Add RewriteUrlInterceptor + add unit test

---
+++ openncp-core/openncp-core-common/src/test/java/eu/europa/ec/sante/openncp/core/common/fhir/interceptors/RewriteUrlInterceptorTest.java
@@ -0,0 +1,68 @@
+package eu.europa.ec.sante.openncp.core.common.fhir.interceptors;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.Constants;
+import eu.europa.ec.sante.openncp.core.common.fhir.context.EuFhirContextFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpHeaders;
+import org.hl7.fhir.r4.model.Attachment;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.DocumentReference;
+import org.hl7.fhir.r4.model.ResourceType;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RewriteUrlInterceptorTest {
+
+
+    @Test
+    void testRewriteAttachmentUrl() throws IOException {
+        final RewriteUrlInterceptor interceptor = new RewriteUrlInterceptor();
+        final MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
+        httpServletRequest.addHeader(HttpHeaders.HOST, "localhost:8091");
+        httpServletRequest.setScheme("http");
+        httpServletRequest.setContextPath("/openncp-client-connector");
+        final FhirContext ctx = EuFhirContextFactory.createFhirContext();
+        final IParser parser = ctx.newJsonParser();
+        final Bundle bundle = parser.parseResource(Bundle.class, IOUtils.toString(
+                this.getClass().getClassLoader().getResourceAsStream("fhir/documentReferences.json"),
+                StandardCharsets.UTF_8));
+        interceptor.rewriteAttachmentUrl(
+                null,
+                null,
+                bundle,
+                httpServletRequest,
+                null);
+        final List<String> rewrittenUrls = bundle.getEntry().stream()
+                .map(Bundle.BundleEntryComponent::getResource)
+                .filter(resource -> resource.getResourceType() == ResourceType.DocumentReference)
+                .map(resource -> (DocumentReference) resource)
+                .map(DocumentReference::getContent)
+                .flatMap(Collection::stream)
+                .map(DocumentReference.DocumentReferenceContentComponent::getAttachment)
+                .filter(attachment -> attachment.getContentType().equalsIgnoreCase(Constants.CT_FHIR_JSON_NEW))
+                .map(Attachment::getUrl)
+                .collect(Collectors.toList());
+        assertThat(rewrittenUrls).hasSize(2).allMatch(url -> url.startsWith(buildReplaceUrl(httpServletRequest)));
+
+    }
+
+    private String buildReplaceUrl(final HttpServletRequest httpServletRequest) {
+        final StringBuilder stringBuilder = new StringBuilder(httpServletRequest.getScheme())
+                .append("://")
+                .append(httpServletRequest.getHeader(HttpHeaders.HOST))
+                .append(httpServletRequest.getContextPath())
+                .append('/');
+        return stringBuilder.toString();
+    }
+}
