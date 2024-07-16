ae93267044f6cd2086875b18f8d21aa257da2215
miorial <alberto.miori@ext.ec.europa.eu>
2024-07-16 23:57:25 +0200

    EHEALTH-11970 - Web-manager doesn't display the anomalies in the anomaly viewer

--- openncp-web-manager/openncp-web-manager-backend/src/main/java/eu/europa/ec/sante/openncp/webmanager/backend/service/AnomalyService.java
+++ openncp-web-manager/openncp-web-manager-backend/src/main/java/eu/europa/ec/sante/openncp/webmanager/backend/service/AnomalyService.java
@@ -1,27 +1,13 @@
 package eu.europa.ec.sante.openncp.webmanager.backend.service;
 
 import eu.europa.ec.sante.openncp.webmanager.backend.persistence.model.Anomaly;
-import eu.europa.ec.sante.openncp.webmanager.backend.persistence.repository.AnomalyRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-@Transactional(readOnly = true)
-public class AnomalyService {
+public interface AnomalyService {
 
-    private final AnomalyRepository anomalyRepository;
 
-    public AnomalyService(AnomalyRepository anomalyRepository) {
-        this.anomalyRepository = anomalyRepository;
-    }
+    List<Anomaly> getAllAnomalies();
 
-    public List<Anomaly> getAllAnomalies() {
-        return anomalyRepository.findAll();
-    }
-
-    public Anomaly getAnomaly(Long id) {
-        return anomalyRepository.findById(id).orElseThrow();
-    }
+    Anomaly getAnomaly(Long id);
 }
