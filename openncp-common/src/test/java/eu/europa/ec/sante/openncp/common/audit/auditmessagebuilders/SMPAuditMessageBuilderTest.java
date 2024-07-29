f1d5a8fb39b5a0ef418d0cd890ad2b3c0be02cf1
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-07-29 16:09:37 +0200

    [EHEALTH-11292] Last implementation fixes based on Gazelle EVS Client validation feedback

--- openncp-common/src/test/java/eu/europa/ec/sante/openncp/common/audit/auditmessagebuilders/SMPAuditMessageBuilderTest.java
+++ openncp-common/src/test/java/eu/europa/ec/sante/openncp/common/audit/auditmessagebuilders/SMPAuditMessageBuilderTest.java
@@ -16,20 +16,18 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Date;
 
-import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
-
 public class SMPAuditMessageBuilderTest extends XMLTestCase {
 
     @Test
     public void testBuild() throws Exception {
 
-        EventLog eventLog = new EventLog();
+        final EventLog eventLog = new EventLog();
         eventLog.setEventType(EventType.SMP_QUERY);
         eventLog.setNcpSide(NcpSide.NCP_A);
         eventLog.setEI_TransactionName(TransactionName.SMP_QUERY);
         eventLog.setEI_EventActionCode(EventActionCode.EXECUTE);
         eventLog.setEI_EventOutcomeIndicator(EventOutcomeIndicator.FULL_SUCCESS);
-        XMLGregorianCalendar now = DateUtil.getDateAsXMLGregorian(new Date());
+        final XMLGregorianCalendar now = DateUtil.getDateAsXMLGregorian(new Date());
         eventLog.setEI_EventDateTime(now);
         eventLog.setSC_UserID("ncp-ppt.pt.ehealth.testa.eu");
         eventLog.setSP_UserID("smp-test.publisher.ehealth.testa.eu");
@@ -38,10 +36,10 @@ public class SMPAuditMessageBuilderTest extends XMLTestCase {
         eventLog.setTargetip("smp-cert-auth-test.publisher.ehealth.testa.eu");
         eventLog.setEventTargetParticipantObjectIds(Collections.singletonList("aHR0cHM6Ly9zbXAtdGVzdC5wdWJsaXNoZXIuZWhlYWx0aC50ZXN0YS5ldS9laGVhbHRoLXBhcnRpY2lwYW50aWQtcW5zJTNBJTNBdXJuJTNBZWhlYWx0aCUzQWF0JTNBbmNwLWlkcA=="));
         eventLog.setEM_ParticipantObjectDetail("errorMessage".getBytes());
-        SMPAuditMessageBuilder smpAuditMessageBuilder = new SMPAuditMessageBuilder();
-        AuditMessage generatedAuditMessage = smpAuditMessageBuilder.build(eventLog);
-        URL url = Resources.getResource("smpqueryauditmessage.xml");
-        AuditMessage expectedAuditMessage = AuditTrailUtils.convertXMLToAuditObject(IOUtils.toInputStream(Resources.toString(url, StandardCharsets.UTF_8)));
+        final SMPAuditMessageBuilder smpAuditMessageBuilder = new SMPAuditMessageBuilder();
+        final AuditMessage generatedAuditMessage = smpAuditMessageBuilder.build(eventLog);
+        final URL url = Resources.getResource("smpqueryauditmessage.xml");
+        final AuditMessage expectedAuditMessage = AuditTrailUtils.convertXMLToAuditObject(IOUtils.toInputStream(Resources.toString(url, StandardCharsets.UTF_8)));
         expectedAuditMessage.getEventIdentification().setEventDateTime(now);
         XMLUnit.setIgnoreWhitespace(true);
         assertXMLEqual(AuditTrailUtils.convertAuditObjectToXML(expectedAuditMessage), AuditTrailUtils.convertAuditObjectToXML(generatedAuditMessage));
@@ -50,13 +48,13 @@ public class SMPAuditMessageBuilderTest extends XMLTestCase {
     @Test
     public void testBuildIncludingError() throws Exception {
 
-        EventLog eventLog = new EventLog();
+        final EventLog eventLog = new EventLog();
         eventLog.setEventType(EventType.SMP_QUERY);
         eventLog.setNcpSide(NcpSide.NCP_A);
         eventLog.setEI_TransactionName(TransactionName.SMP_QUERY);
         eventLog.setEI_EventActionCode(EventActionCode.EXECUTE);
         eventLog.setEI_EventOutcomeIndicator(EventOutcomeIndicator.FULL_SUCCESS);
-        XMLGregorianCalendar now = DateUtil.getDateAsXMLGregorian(new Date());
+        final XMLGregorianCalendar now = DateUtil.getDateAsXMLGregorian(new Date());
         eventLog.setEI_EventDateTime(now);
         eventLog.setSC_UserID("ncp-ppt.pt.ehealth.testa.eu");
         eventLog.setSP_UserID("smp-test.publisher.ehealth.testa.eu");
@@ -66,13 +64,12 @@ public class SMPAuditMessageBuilderTest extends XMLTestCase {
         eventLog.setEventTargetParticipantObjectIds(Collections.singletonList("aHR0cHM6Ly9zbXAtdGVzdC5wdWJsaXNoZXIuZWhlYWx0aC50ZXN0YS5ldS9laGVhbHRoLXBhcnRpY2lwYW50aWQtcW5zJTNBJTNBdXJuJTNBZWhlYWx0aCUzQWF0JTNBbmNwLWlkcA=="));
         eventLog.setEM_ParticipantObjectID("aHR0cHM6Ly9zbXAtY2VydC1hdXRoLXRlc3QucHVibGlzaGVyLmVoZWFsdGgudGVzdGEuZXUvZWhlYWx0aC1wYXJ0aWNpcGFudGlkLXFuczo6dXJuOmVoZWFsdGg6cHQ6bmNwLWlkcC9zZXJ2aWNlcy9laGVhbHRoLXJlc2lkLXFuczo6dXJuOmVoZWFsdGg6cGF0aWVudGlkZW50aWZpY2F0aW9uYW5kYXV0aGVudGljYXRpb246OnhjcGQ6OmNyb3NzZ2F0ZXdheXBhdGllbnRkaXNjb3ZlcnklMjMlMjNpdGktNTU=");
         eventLog.setEM_ParticipantObjectDetail("errorMessage".getBytes());
-        SMPAuditMessageBuilder smpAuditMessageBuilder = new SMPAuditMessageBuilder();
-        AuditMessage generatedAuditMessage = smpAuditMessageBuilder.build(eventLog);
-        URL url = Resources.getResource("smpqueryauditmessagewitherror.xml");
-        AuditMessage expectedAuditMessage = AuditTrailUtils.convertXMLToAuditObject(IOUtils.toInputStream(Resources.toString(url, StandardCharsets.UTF_8),StandardCharsets.UTF_8));
+        final SMPAuditMessageBuilder smpAuditMessageBuilder = new SMPAuditMessageBuilder();
+        final AuditMessage generatedAuditMessage = smpAuditMessageBuilder.build(eventLog);
+        final URL url = Resources.getResource("smpqueryauditmessagewitherror.xml");
+        final AuditMessage expectedAuditMessage = AuditTrailUtils.convertXMLToAuditObject(IOUtils.toInputStream(Resources.toString(url, StandardCharsets.UTF_8), StandardCharsets.UTF_8));
         expectedAuditMessage.getEventIdentification().setEventDateTime(now);
         XMLUnit.setIgnoreWhitespace(true);
-        System.out.println(AuditTrailUtils.convertAuditObjectToXML(generatedAuditMessage));
         assertXMLEqual(AuditTrailUtils.convertAuditObjectToXML(expectedAuditMessage), AuditTrailUtils.convertAuditObjectToXML(generatedAuditMessage));
     }
 }
