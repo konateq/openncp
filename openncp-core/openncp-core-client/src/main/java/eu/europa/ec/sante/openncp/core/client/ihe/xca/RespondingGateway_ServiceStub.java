85fdba86b00b46fadc5b978ffff8d6dcc4872a16
chandsr <srinivas.chandragiri@ext.ec.europa.eu>
2024-08-01 14:42:36 +0200

    EHEALTH-10200 - Fixed the bug.

--- openncp-core/openncp-core-client/src/main/java/eu/europa/ec/sante/openncp/core/client/ihe/xca/RespondingGateway_ServiceStub.java
+++ openncp-core/openncp-core-client/src/main/java/eu/europa/ec/sante/openncp/core/client/ihe/xca/RespondingGateway_ServiceStub.java
@@ -424,12 +424,15 @@ public class RespondingGateway_ServiceStub extends Stub {
 
                     /* add the new message context to the new operation client */
                     newOperationClient.addMessageContext(newMessageContext);
-                    /* we retry the request */
-                    newOperationClient.execute(true);
+
                     /* we need to reset the previous variables with the new content, to be used later */
                     _operationClient = newOperationClient;
                     _messageContext = newMessageContext;
                     env = newEnv;
+
+                    /* we retry the request */
+                    newOperationClient.execute(true);
+
                     LOGGER.debug("Successfully retried the request! Proceeding with the normal workflow...");
                 } else {
                     /* if we cannot solve this issue through the Central Services, then there's nothing we can do, so we let it be thrown */
@@ -814,12 +817,13 @@ public class RespondingGateway_ServiceStub extends Stub {
 
                     /* add the new message context to the new operation client */
                     newOperationClient.addMessageContext(newMessageContext);
-                    /* we retry the request */
-                    newOperationClient.execute(true);
+
                     /* we need to reset the previous variables with the new content, to be used later */
                     _operationClient = newOperationClient;
                     _messageContext = newMessageContext;
                     env = newEnv;
+                    /* we retry the request */
+                    newOperationClient.execute(true);
                     LOGGER.debug("Successfully retried the request! Proceeding with the normal workflow...");
                 } else {
                     /* if we cannot solve this issue through the Central Services, then there's nothing we can do, so we let it be thrown */
